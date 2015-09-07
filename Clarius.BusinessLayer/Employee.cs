b9a921fc0c6bae05f87afb94951e7bea03a4573b
Herman Gouw <hermangouw@gmail.com>
2015-09-08 01:44:19 +1000

    Initial Version

---
+++ Clarius.BusinessLayer/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace Clarius.BusinessLayer
+{
+    public class Employee
+    {
+        [Key]
+        public string EmployeeNo { get; set; } // 32
+        public string Description { get; set; } // 62
+        public string CommonName { get; set; } // 30
+        public DateTime DateOfBirth { get; set; }
+        public string EmailAddress { get; set; } // 100
+        public DateTime DateEmployed { get; set; }
+        public DateTime DateTerminated { get; set; }
+        public string ReasonSuspension { get; set; } // 32
+        public string ReasonTermination { get; set; } // 32
+        public string Occupation { get; set; } // 32
+        public short EmployeeStatus { get; set; }
+        public short Class { get; set; }
+        public decimal RateOfPay { get; set; }
+        public decimal NetPay { get; set; }
+
+        //public int Age { get; set; }
+    }
+
+    public class ClariusContex : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
